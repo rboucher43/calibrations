82d8bcf415ab8d760c908117528ab6f8accc9d88
Reese E Boucher <rboucher43@rcas2066.rcf.bnl.gov>
2023-07-17 22:36:15 -0400

    Delete obsolete files

--- TrackerMillepedeAlignment/process_mille_macros/easy_make_steering.sh
+++
@@ -1,3 +0,0 @@
-cat /sphenix/tg/tg01/hf/rboucher43/eval_output/*_mille_steer_helical.txt > /sphenix/tg/tg01/hf/rboucher43/eval_output/mille_steer_helical.txt
-echo "mille_vertex_constraints.txt" >> mille_steer_helical.txt
-echo "mvtx_constraints_sensor_stave.txt" >> mille_steer_helical.txt
